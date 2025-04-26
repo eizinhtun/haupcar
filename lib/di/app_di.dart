e5caa783d91a4a9f41ad7337675d6210f7fb2dc5
eizinhtun <eizinzinlin@gmail.com>
2025-04-27 02:15:27 +0700

    update gradle wrapper

--- lib/di/app_di.dart
+++ lib/di/app_di.dart
@@ -3,12 +3,21 @@ import 'package:haupcar/data/data.dart';
 import 'package:haupcar/domain/domain.dart';
 import 'package:haupcar/presentation/presentation.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/util/legacy_to_async_migration_util.dart';
 
 GetIt getIt = GetIt.instance;
 
 Future configureDependencies() async {
-  getIt.registerSingleton<SharedPrefs>(
-      SharedPrefs(await SharedPreferences.getInstance()));
+  const SharedPreferencesOptions sharedPreferencesOptions =
+      SharedPreferencesOptions();
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await migrateLegacySharedPreferencesToSharedPreferencesAsyncIfNecessary(
+    legacySharedPreferencesInstance: prefs,
+    sharedPreferencesAsyncOptions: sharedPreferencesOptions,
+    migrationCompletedKey: 'migrationCompleted',
+  );
+  getIt.registerSingleton<SharedPrefs>(SharedPrefs(prefs));
+
   getIt.registerLazySingleton<NetworkService>(() => NetworkService.instance());
 
   // await Hive.openBox(RECIPE_CACHE_BOX_NAME);
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:haupcar/core/core.dart';
 import 'package:haupcar/presentation/presentation.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Hive.initFlutter();
+
   await configureDependencies();
 
   runApp(const App());
